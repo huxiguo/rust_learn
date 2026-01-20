9c7bec58cce8d38746319a83dd6d0c81916547f0
seekhoo <hu@seekhoo.cn>
2026-01-20 17:22:10 +0800

    add single demo

---
+++ src/demo/guessing_game.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+/**
+ * 猜数字游戏
+ */
+pub fn run() {
+    println!("Guess the number!");
+    // 生成一个1到100之间的随机数
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Please input your guess and press Enter.");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Please type a number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! The secret number is {secret_number}");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod demo;
+
+// 主函数
+fn main() {
+    println!("Hello, world!");
+    demo::guessing_game::run();
+}
